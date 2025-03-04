63e3c357cf3a772cc3be3ee4f777d9e0ef1af439
barry952 <rabinpoudel2004@gmail.com>
2025-03-04 13:38:50 +0545

    pushed

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,jsx,ts,tsx}",  // Ensure this covers all your component files
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
